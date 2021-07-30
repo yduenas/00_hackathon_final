c6408f28ae6ec0747cf3c9cb2232da8a8ff38df8
Ytalo Duenas <ytaloduenas@gmail.com>
2021-07-30 11:33:31 -0500

    agregue boton temporal para ir al login

--- src/views/Pachacutec.vue
+++ src/views/Pachacutec.vue
@@ -1,22 +1,25 @@
 <template>
-  <div>
-    <Home />
-    <Programas />
-    <Comunidad />
-  </div>
+	<div>
+		<router-link to="/login" type="submit" class="form-control ingresar"
+			>Ir al login
+		</router-link>
+		<Home />
+		<Programas />
+		<Comunidad />
+	</div>
 </template>
 
 <script>
-import Home from "@/components/Home.vue";
-import Programas from "@/components/Programas.vue";
-import Comunidad from "@/components/Comunidad.vue";
+import Home from '@/components/Home.vue';
+import Programas from '@/components/Programas.vue';
+import Comunidad from '@/components/Comunidad.vue';
 export default {
-  name: "Pachacutec",
-  components: {
-    Home,
-    Programas,
-    Comunidad,
-  },
+	name: 'Pachacutec',
+	components: {
+		Home,
+		Programas,
+		Comunidad,
+	},
 };
 </script>
 
