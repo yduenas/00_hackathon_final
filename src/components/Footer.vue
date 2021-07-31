0845d79ae0e85c5fd0db7a847af429fa4bc793b5
Ytalo Duenas <ytaloduenas@gmail.com>
2021-07-30 22:42:45 -0500

    corregi scoped componente beneficios

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,35 @@
 <template>
-  <section class="contenedor">
-    <div class="footer">
-      <div>Con el respaldo de:</div>
-      <div>
-        <img class="img" src="../assets/img/logo_intercorp_white.svg" alt="" />
-        <img class="img" src="../assets/img/logo_idat_white.svg" alt="" />
-        <img class="img" src="../assets/img/logo_zegel_white.svg" alt="" />
-      </div>
-    </div>
-  </section>
+	<section class="contenedor">
+		<div class="footer">
+			<div>Con el respaldo de:</div>
+			<div>
+				<img class="img" src="../assets/img/logo_intercorp_white.svg" alt="" />
+				<img class="img" src="../assets/img/logo_idat_white.svg" alt="" />
+				<img class="img" src="../assets/img/logo_zegel_white.svg" alt="" />
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style scoped>
 .footer {
-  color: white;
-  background: #000d29;
-  width: 100%;
-  padding: 1.5em;
-  text-align: left;
-  font-size: 10px;
+	color: white;
+	background: #000d29;
+	width: 100%;
+	padding: 1.5em;
+	text-align: left;
+	font-size: 10px;
 }
 .img {
-  padding: 0.5em 1em 0 0;
+	padding: 0.5em 1em 0 0;
 }
 @media (min-width: 361px) {
-  .img {
-    padding-right: 2em;
-  }
+	.img {
+		padding-right: 2em;
+	}
 }
 </style>
